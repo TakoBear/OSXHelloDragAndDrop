6f2096718645ec11ea43da3997567e869dc781a3
Bear <timfanda35@gmail.com>
2014-05-10 17:07:06 +0800

    Initial Commit

---
+++ HelloDragAndDrop/bearAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  bearAppDelegate.m
+//  HelloDragAndDrop
+//
+//  Created by Bear on 2014/5/10.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import "bearAppDelegate.h"
+
+@implementation bearAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ HelloDragAndDrop/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  HelloDragAndDrop
+//
+//  Created by Bear on 2014/5/10.
+//  Copyright (c) 2014年 TakoBear. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
